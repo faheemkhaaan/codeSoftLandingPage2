e1c2da5e33b82c032c2e10821594a1046e3fdff4
faheemkhaaan <faheemkhan.pg@gmail.com>
2024-03-28 14:27:07 -0700

    Fixed mobile responsiveness

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
- 
+const hambuger = document.querySelector(".hambuger");
+const navBar = document.querySelector(".navBar");
+
+hambuger.onclick = function () {
+    navBar.classList.toggle("visible")
+}
